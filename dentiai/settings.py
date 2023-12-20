72ed3d339b07a2439e93cc07cb0fdcd6c643727f
Paul Pavlov <dev.captainmurphy@gmail.com>
2023-12-21 00:09:57 +0400

    Update settings.py

--- dentiai/settings.py
+++ dentiai/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "treebeard",
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = "dentiai.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "dentiai",
+        "USER": "pp",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
